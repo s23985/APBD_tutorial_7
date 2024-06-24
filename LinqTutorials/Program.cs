8137e02f138ddc1584b4efdab5154689e2e7897c
s23985 <s23985@pjwstk.edu.pl>
2024-06-24 23:00:18 +0200

    Project initialization

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+
+            var t1 = LinqTasks.Task2();
+
+        }
+    }
+}
